166df6a5b6d48963d6e0099dff8a36c22bea18dc
ausaafnabi <nabiausaaf@gmail.com>
2020-05-09 21:26:33 +0530

    Added Gallery | Fixed Admin

--- templates/admin/listArticles.php
+++ templates/admin/listArticles.php
@@ -1,10 +1,17 @@
 <?php include "templates/Inc/Header.php" ?>
-
+	<style>
+	.table-responsive {
+			max-height: 300px;
+		}
+	</style>
+	
+	<div class="container">
+		
       <div id="adminHeader">
         <h2>Widget News Admin</h2>
         <p>You are logged in as <b><?php echo htmlspecialchars( $_SESSION['username']) ?></b>. <a href="admin.php?action=logout"?>Log out</a></p>
       </div>
-
+	<div class="card col-md-5">
       <h1>All Articles</h1>
 
 <?php if ( isset( $results['errorMessage'] ) ) { ?>
@@ -15,12 +22,14 @@
 <?php if ( isset( $results['statusMessage'] ) ) { ?>
         <div class="statusMessage"><?php echo $results['statusMessage'] ?></div>
 <?php } ?>
-
-      <table>
+	<div style="display: block;overflow-y:scroll;height:300px;" class="table-responsive">
+      <table class="table table-hover">
+       <thread>
         <tr>
           <th>Publication Date</th>
           <th>Article</th>
         </tr>
+       </thread>
 
 <?php foreach ( $results['articles'] as $article ) { ?>
 
@@ -34,9 +43,10 @@
 <?php } ?>
 
       </table>
-
+</div>
       <p><?php echo $results['totalRows']?> article<?php echo ( $results['totalRows'] != 1 ) ? 's' : '' ?> in total.</p>
-
-      <p><a href="admin.php?action=newArticle">Add a New Article</a></p>
-
+	
+    <a class="btn btn-primary px-3 px-2" href="admin.php?action=newArticle">Add a New Article</a>
+	</div>
+</div>
 <?php include "templates/Inc/Footer.php" ?>
--- templates/admin/loginForm.php
+++ templates/admin/loginForm.php
@@ -1,30 +1,44 @@
 <?php include "templates/Inc/Header.php" ?>
 
-      <form action="admin.php?action=login" method="post" style="width: 50%;">
-        <input type="hidden" name="login" value="true" />
 
-<?php if ( isset( $results['errorMessage'] ) ) { ?>
-        <div class="errorMessage"><?php echo $results['errorMessage'] ?></div>
-<?php } ?>
-
-        <ul>
-
-          <li>
-            <label for="username">Username</label>
-            <input type="text" name="username" id="username" placeholder="Your admin username" required autofocus maxlength="20" />
-          </li>
-
-          <li>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" placeholder="Your admin password" required maxlength="20" />
-          </li>
-
-        </ul>
-
-        <div class="buttons">
-          <input type="submit" name="login" value="Login" />
+    <div class="owl-carousel loop-block-31 ">
+      <div class="block-30 block-30-sm item" style="background-image: url('images/bg_1.jpg');" data-stellar-background-ratio="0.5">
+        <div class="container">
+          <div class="row align-items-center justify-content-center text-center">
+            <div class="col-md-4">
+              	<div class="card fundraise-item">
+					<div class="form volunteer" style="padding-left:20px;">
+					<form action="admin.php?action=login" method="post" style="width: 90%;">
+						<div class="form-group"></div>
+							<img style="height: 100px;width: 100px;display: inline;" src="images/black-trans.png" alt="Image placeholder">
+						<input type="hidden" name="login" value="true" />
+							<?php if ( isset( $results['errorMessage'] ) ) { ?>
+								<div class="errorMessage"><?php echo $results['errorMessage'] ?></div>
+							<?php } ?>
+									<div class="form-group">
+										<div style="margin-bottom: 25px" class="input-group">
+											<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+											<input type="text" name="username" id="username" class="form-control py-2" placeholder="Admin Username" required autofocus maxlength="20" />
+										</div>
+									</div>
+									
+									<div class="form-group">
+									<div style="margin-bottom: 25px" class="input-group">
+									 <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+										<input type="password" name="password" id="password" class="form-control py-2" placeholder="Admin Password" required maxlength="20" />
+									</div>
+									</div>
+							<div class="form-group">
+								<div class="buttons">
+									<input type="submit" class="btn btn-primary px-3 py-2" style="background: #f7ca44;border: 2px solid #f7ca44; color:#fff" name="login" value="Login" />
+								</div>
+							</div>
+					</form>
+					</div>
+				</div>
+            </div>
+          </div>
         </div>
-
-      </form>
-
+      </div>
+  </div>
 <?php include "templates/Inc/Footer.php" ?>
