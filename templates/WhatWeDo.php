3fbdc9ed4d7e0d13f0d6203852a7bd0185541df8
ausaafnabi <nabiausaaf@gmail.com>
2020-05-17 21:40:41 +0530

    Update WhatWeDo.php [#1]

--- templates/WhatWeDo.php
+++ templates/WhatWeDo.php
@@ -18,30 +18,34 @@
 <div class="site-section mb-5">
     <div class="container">
       <div class="row mb-5">
-        <div class="col-md-12 mb-5"><h1>We At Enactus</h1></div>
+        <div class="col-md-12 mb-5"><h1>Who Are We ?</h1></div>
 			<div class="col-md-6">
-				<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, necessitatibus officiis facere nisi et, ut adipisci a quis quisquam vitae doloremque tempora repellat quae accusantium atque eum voluptatibus aperiam cumque.</p>
-				<p>Quia ratione, eum harum ab similique mollitia, nisi itaque vel voluptas ipsam dolore perferendis. Deleniti voluptatum error possimus ipsum, sed, obcaecati. Sit unde quia eum repudiandae molestiae reprehenderit harum nesciunt.</p>   
+				<p class="lead">Enactus, formerly known as Students in Free Enterprise (SIFE), is an international Not-for-Profit organisation steadfast to unleash the next generation of entrepreneurial leaders to use innovation and business principles to ameliorate the world. </p>
+				<p>The Enactus network of global business, academic and student leaders is unified by its vision- to create a better, more sustainable world.</p>   
 			</div>
 			<div class="col-md-6">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, necessitatibus officiis facere nisi et, ut adipisci a quis quisquam vitae doloremque tempora repellat quae accusantium atque eum voluptatibus aperiam cumque.</p>
-				<p>Quia ratione, eum harum ab similique mollitia, nisi itaque vel voluptas ipsam dolore perferendis. Deleniti voluptatum error possimus ipsum, sed, obcaecati. Sit unde quia eum repudiandae molestiae reprehenderit harum nesciunt.</p>
+				<p class="lead">Enactus students use the propitious power of business, innovation and entrepreneurial action to address the most pertinent issues affecting the world through diverse and fresh perspectives while empowering people and creating livelihood.</p>
+				<p>Today, it is successfully operating in 36 countries, significantly impacting the lives of 1.3 million people each year across the globe.</p>
 			</div>
 		</div>
 	</div>  
-    <div class="site-section fund-raisers bg-light">
-		<div class="container">
-			<div class="row mb-3 justify-content-center">
-				<div class="col-md-8 text-center">
-					<h2>Latest Articles</h2>
-					<p class="lead">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-					<p><a href="#" class="link-underline">View All Articles</a></p>
-				</div>
-			</div>
-		</div>    
-	</div>
-    
 
+	<div class="container">
+      <div class="row mb-5">
+        <div class="col-md-12 mb-5"><h1>We At Enactus</h1></div>
+			<div class="col-md-12 mb-5">
+				<p class="lead">Enactus Aryabhatta was established in 2016. Since its inception, the society has worked towards bringing about a formidable change in the lives of various under-privileged sections of the society while concurrently developing entrepreneurial leaders and social innovators of tomorrow. With a team of 60+ students, academic and business leaders, we have consolidated the essence of Enactus, to create a better, more sustainable world through our diverse community outreach projects.</p>
+			</div>
+				<div class="col-md-6" style="padding-left:0px;">
+				<p class="lead">Enactus Aryabhatta has taken up ___community outreach projects till date, of which __have been consummated and 3 are continuing. Our three running projects are Project Utkarsh, Project Palaash and Project Suvidha. </p>   
+				</div>
+				
+				<div class="col-md-6" style="padding-left:0px;">
+				<p class="lead" style="margin-bottom=0.1rem;">Enactus Aryabhatta is a staunch proponent of transforming and nurturing entrepreneurial action because we solemnly believe in the dictum,<blockquote class="blockquote">"We rise by lifting others."</blockquote></p>   
+				</div>
+
+		</div>
+	</div>  
 
 
 <?php include 'Inc/Footer.php' ?>
