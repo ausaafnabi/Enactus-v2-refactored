48146cfe7c2864b52d1b5a80a05ddb1e1b3d4401
ausaafnabi <nabiausaaf@gmail.com>
2020-05-20 21:32:19 +0530

    created Project page: Utkarsh

---
+++ templates/Projects/Utkarsh.php
@@ -0,0 +1,90 @@
+<?php include 'templates/Inc/Header.php'?>
+<div class="block-31" style="position: relative;">
+    <div class="owl-carousel loop-block-31 ">
+      <div class="block-30 block-30-sm item" style="background-image: url('images/07-min.JPG');" data-stellar-background-ratio="0.5">
+        <div class="container">
+          <div class="row align-items-center justify-content-center text-center">
+            <div class="col-md-7">
+              <h2 class="heading mb-5">Project<br>Utkarsh</h2>
+              <p style="display: inline-block;"><a href="https://youtu.be/H3gDZgewJGY"  data-fancybox class="ftco-play-video d-flex"><span class="play-icon-wrap align-self-center mr-4"><span class="ion-ios-play"></span></span> <span class="align-self-center">Watch Video</span></a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="block-30 block-30-sm item" style="background-image: url('images/Team.jpg');" data-stellar-background-ratio="0.5">
+        <div class="container">
+          <div class="row align-items-center justify-content-center text-center">
+            <div class="col-md-7">
+              <h2 class="heading mb-5">Project<br>Utkarsh</h2>
+              <p style="display: inline-block;"><a href="https://youtu.be/H3gDZgewJGY"  data-fancybox class="ftco-play-video d-flex"><span class="play-icon-wrap align-self-center mr-4"><span class="ion-ios-play"></span></span> <span class="align-self-center">Watch Video</span></a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="site-section mb-5">
+    <div class="container">
+      <div class="row mb-5">
+        <div class="col-md-12 mb-5"><h1 style="color:#f27506;">Project Utkarsh</h1></div>
+        <div class="col-md-12 "><blockquote class="quote-card yellow-card"><p style="color:#fff;">A vision to uplift downtrodden women by giving them a source to earn through the means of their existing skill of cooking and further attaching  to it a newfound business knowledge.</p></blockquote></div>
+        <div class="col-md-12 mb-2">
+			<h2>The Problem</h2>
+			<h4 style="margin-bottom:0.1rem;">Gender Bias</h4>
+            <p class="lead">For ages, women have been subjugated by a patriarchal society and intransigent notions against them, shunned as incapable of being financially independent. It comes as no surprise that women make up a mere 27% of the total workforce in our country.</p>
+			<h4 style="margin-bottom:0.1rem;">Food Wastage</h4>
+            <p class="lead">It is estimated that food worth ₹50,000 crore is wasted every year in our country, most of which could be used to feed the hunger-stricken.</p>
+			<h4 style="margin-bottom:0.1rem;">Surge of Fast Food Consumption</h4>
+            <p class="lead">An unfaltering rise in unhealthy eating and snacking habits is the leading cause of innumerable health issues like high blood pressure, cholesterol and dehydration.</p>
+        </div>
+        <div class="col-md-12 mb-2">
+			<h2>Our Initiatives</h2>
+            <p class="lead">Project Utkarsh simultaneously tackles the aforementioned problems by employing underprivileged women and utilising the unused portions of fruits and vegetables procured from juice shops or Safal outlets to produce healthy chips, preventing food wastage and generating a healthier snacking alternative taking into consideration the rise in health issues of the consumers.</p>  
+            <h3>Goals</h3>
+			<ul>
+				<li>
+					<p class="lead"><strong>Empower : </strong>Women burdened with domestic hardships by transforming their cooking skills into a means to earn and grow. </p>
+				</li>
+				<li>
+					<p class="lead"><strong>Salvage : </strong>Unutilized portions of fruits and vegetables, that are usually highly nutritious yet are discarded as waste. </p>
+				</li>
+				<li>
+					<p class="lead"><strong>Promote : </strong>Healthy eating habits by introducing a nutritious alternative to fat-ridden potato chips. </p>
+				</li>
+			</ul>
+		  </div>
+		  <div class="col-md-12 mb-2">
+			<h2>Community</h2>
+			<p class="lead">Our target community is the underprivileged women of our society belonging to Delhi’s ugly underbelly, right amidst shiny malls and sprawling bungalows — Kusumpur Pahari village near Vasant Vihar, Ravidas Camp, R.K. Puram and Todapur Village, Naraina. These women, apart from being the sole bearers of their families’ domestic burden, are subjected to the ramifications of an ever-present patriarchal mindset, stunted financial growth owing to the age-long dogma of men being the sole breadwinners of the family, and extremely unsanitary and oftentimes hazardous living conditions. </p>
+		  </div>  
+		  <div class="col-md-12 mb-2">
+			<h2>Business Model</h2>
+            <p class="lead">The entire entrepreneurial action in our project begins right from the procurement of our raw material. During the project development phase we identified an opportunity to convert the carrot pulp from juice shops into something beneficial for the society. The carrot pulp is procured from juice shops through a proper model of segregation. Thereafter, a standard recipe is followed by our lady entrepreneurs to manufacture Veggitos. The recipe was developed keeping in mind the need to fulfil the market gap for a healthier snacking alternative to potato chips. The chips prepared by us are 50% less in fat and 14% less in calories.</p>  
+			<p class="lead">Our product is manufactured conforming to a well-defined hygiene module, making our chips absolutely safe to consume.  Our baked munchies are packaged in recycled paper bags. This packaging gives them a premium look and also is environment friendly. Once baked and packaged, Veggitos are ready to hit the shelves of various supermarket stores. Alongside, we focus on a large scale marketing campaign for Veggitos, in order to make potential customers aware of our product.</p>
+		  </div> 
+		  <div class="col-md-12 mb-2">
+			  
+			<h2 class="mb-3">Impact</h2>
+			<ul>
+				<li>
+					<h3 style="margin-bottom:0.1rem;">Social</h3>
+					<p class="lead" >Through our persistent efforts, we have guided our lady entrepreneurs through the path of financial independence further equipping them to actively contribute to their personal as well as societal welfare. Now being proactive citizens of their community, they participate in important decision-making affairs and reach out to form networks wherein both the society and their interests co-exist.</p>
+					<p>The increase in the lady entrepreneurs’ family income has allowed the children of their family to gain access to better educational facilities. </p>
+					<p>Utkarsh has not only enhanced their cooking skills but also given them a resolve to continue learning. 8 of our lady entrepreneurs went on to the extent of resuming their unfinished education.</p>
+					<p>Lastly, Utkarsh benefits the society as a whole as Veggitos offer customers a healthy and indulgent alternative to fried and unhealthy chips. </p>
+				</li>
+				<li>
+					<h3 style="margin-bottom:0.1rem;">Environmental</h3>
+					<p class="lead" >We have positively impacted the environment by reducing food and water wastage. Through Project Utkarsh, we have saved over 90,000 gallons of water and over 1200 kgs of wasted food.</p>
+				</li>
+				<li>
+					<h3 style="margin-bottom:0.1rem;">Economic</h3>
+					<p class="lead" >We have successfully entered a global market of potato chips of the size of US$ 28 billion by making over 14,000 packets of Veggitos generating a revenue of over ₹7,20,000 in the process. We have empowered over 50 lady entrepreneurs by providing them with a source to learn and earn through their already existing skill of cooking, causing a 200% increase in their family income. </p>
+				</li>
+			</ul>
+          </div>
+          <div class="row mt-5 justify-content-center"><img src="images/Utkarsh_info.jpeg" alt="" style="max-width: 70%;height:auto;"></div>
+		</div>
+	</div>
+ </div>
+<?php include 'templates/Inc/Footer.php'?>
