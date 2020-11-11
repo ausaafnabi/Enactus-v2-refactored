85dcaa13997693e096ec663a60f8925e9e96abec
ausaafnabi <nabiausaaf@gmail.com>
2020-11-11 20:41:56 +0530

    [Update] Added New Cores section

--- templates/Team.php
+++ templates/Team.php
@@ -26,21 +26,91 @@
     <div class="container">
       <div class="row mb-5">
         <div class="col-md-12 mb-5"><h1>Our Team</h1></div>
-        <div class="col-md-12 "><blockquote class="quote-card yellow-card"><p>Coming together is a beginning, staying together is progress, and working together is success.</p><cite>Henry Ford</cite></blockquote></div>
-        <div class="col-md-6">
-            <p class="lead">Our team, at Enactus Aryabhatta, works with utmost dedication and diligence to strive towards common objectives and work tremendously to achieve the target of 17 Sustainable Development Goals set by the United Nations.</p>
-            <p>We believe that some of the most remarkable impacts are made by collaborative efforts of a team. Teamwork executes a network which enlists enthusiasm by building a strong foundation of team spirit determined to overcome the challenges faced by the nation.</p>
-            
-          </div>
-          <div class="col-md-6">
-            <p>To foster the holistic development of our team, we hold a number of Community Sessions, wherein, we engage with the localities and merge our entrepreneurial ideas to tackle innumerable social issues faced by the community. Thereby, advancing our work towards improving lives, strengthening communities and developing a sustainable society. </p>
-            <p>The team of Enactus Aryabhatta has upheld 6 Community outreach projects with <strong>140+</strong> students working for a total of <strong>7000+</strong>  Community hours. These projects had around <strong>800+</strong> lives impacted annually. </p>
-          </div>
-      </div>
-      
-      <div class="row mt-5 justify-content-center">
+			<div class="col-md-12 "><blockquote class="quote-card yellow-card"><p>Coming together is a beginning, staying together is progress, and working together is success.</p><cite>Henry Ford</cite></blockquote></div>
+			<div class="col-md-6">
+				<p class="lead">Our team, at Enactus Aryabhatta, works with utmost dedication and diligence to strive towards common objectives and work tremendously to achieve the target of 17 Sustainable Development Goals set by the United Nations.</p>
+				<p>We believe that some of the most remarkable impacts are made by collaborative efforts of a team. Teamwork executes a network which enlists enthusiasm by building a strong foundation of team spirit determined to overcome the challenges faced by the nation.</p>
+			</div>
+			<div class="col-md-6">
+				<p>To foster the holistic development of our team, we hold a number of Community Sessions, wherein, we engage with the localities and merge our entrepreneurial ideas to tackle innumerable social issues faced by the community. Thereby, advancing our work towards improving lives, strengthening communities and developing a sustainable society. </p>
+				<p>The team of Enactus Aryabhatta has upheld 6 Community outreach projects with <strong>140+</strong> students working for a total of <strong>7000+</strong>  Community hours. These projects had around <strong>800+</strong> lives impacted annually. </p>
+			</div>
+		</div>      
+            <div class="row mt-5 justify-content-center">
         <div class="col-md-12 mb-5 text-center mt-5">
           <h2>Core Members</h2>
+        </div>
+         <div class="col-md-6 col-lg-3">
+          <div class="block-38 text-center">
+            <div class="block-38-img">
+              <div class="block-38-header">
+                <img src="images/thumbnail/Ayush.png" alt="Image placeholder" class="rounded-circle" style="width:230px;height:225px">
+                <h3 class="block-38-heading">Sahej Kapoor</h3>
+                <p class="block-38-subheading">President</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-3">
+          <div class="block-38 text-center">
+            <div class="block-38-img">
+              <div class="block-38-header">
+                <img src="images/thumbnail/Sarthak.png" alt="Image placeholder" class="rounded-circle" style="width:230px;height:225px" >
+                <h3 class="block-38-heading">Khushi Joshi</h3>
+                <p class="block-38-subheading">Vice President</p>
+              </div>
+            </div>
+          </div>
+        </div>
+		<div class="col-md-6 col-lg-3">
+          <div class="block-38 text-center">
+            <div class="block-38-img">
+              <div class="block-38-header">
+                <img src="images/thumbnail/NitishGupta.png" alt="Image placeholder" class="rounded-circle" style="width:230px;height:225px">
+                <h3 class="block-38-heading">Adity</h3>
+                <p class="block-38-subheading">Design and Tech Head</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-3">
+          <div class="block-38 text-center">
+            <div class="block-38-img">
+              <div class="block-38-header">
+                <img src="images/thumbnail/AnanyaAwasthi.png" alt="Image placeholder" class="rounded-circle" style="width:230px;height:225px">
+                <h3 class="block-38-heading">Mohit rohilla</h3>
+                <p class="block-38-subheading">Project Head Palaash</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      <div class="col-md-6 col-lg-3">
+          <div class="block-38 text-center">
+            <div class="block-38-img">
+              <div class="block-38-header">
+                <img src="images/thumbnail/Mayank.png" alt="Image placeholder" class="rounded-circle" style="width:230px;height:225px">
+                <h3 class="block-38-heading">Romanna Aamir</h3>
+                <p class="block-38-subheading">Research and Development Head</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-3">
+          <div class="block-38 text-center">
+            <div class="block-38-img">
+              <div class="block-38-header">
+                <img src="images/thumbnail/SarthakSaxena.png" alt="Image placeholder" class="rounded-circle" style="width:230px;height:225px">
+                <h3 class="block-38-heading">Tanushree Arora</h3>
+                <p class="block-38-subheading">Manager</p>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+      <div class="row mt-5 justify-content-center">
+        <div class="col-md-12 mb-5 text-center mt-5">
+          <h2>Ex-Core Members</h2>
         </div>
          <div class="col-md-6 col-lg-3">
           <div class="block-38 text-center">
@@ -122,5 +192,5 @@
         </div>
     </div>
   </div>
- </div>
+
 <?php include "templates/Inc/Footer.php" ?>
